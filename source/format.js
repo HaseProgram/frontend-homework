d28aaf240f5457f7f3e7bb45906774bdb3496ef3
Dmitry <haseprogram@mail.ru>
2018-02-04 00:24:16 +0300

    Fixed some code issues ~~

--- source/format.js
+++ source/format.js
@@ -3,18 +3,23 @@
 /**
 	* Найти максимальный размер каждой колонки
 	*
-	* @param {Array} input - Исходный массив
+	* @param {Array[string]} input - Исходный массив
 	* @param {number} columns - Количество колонок
-	* @return {Array} - Массив, определяет размер каждой колонки
+	* @return {Array[number]} - Массив, определяет размер каждой колонки
 */
 const findLength = function(input, columns) {
-	let columnsLength = [];
-	for(let i = 0; i < input.length; i++) {
-		const len = String(input[i]).length;
-		const col = i % columns;
-		(columnsLength[col] >= len) ? '' : columnsLength[col] = len;
-	}
-	return columnsLength;
+	let columnsLengths = [];
+	input.forEach((item, index) => {
+		const len = item.length;
+		const col = index % columns;
+		
+		if (!columnsLengths[col]) {
+			columnsLengths[col] = len;
+		} else {
+			columnsLengths[col] = Math.max(columnsLengths[col], len);
+		}
+	});
+	return columnsLengths;
 }
 
 /**
@@ -22,35 +27,40 @@ const findLength = function(input, columns) {
 	*
 	* @param {string} data - Исходный элемент массива
 	* @param {number} requiredLength - Максимальная длина колонки
-	* @param {boolean} notFirst - Находится ли элемент в первой колонке (для разделения)
+	* @param {boolean} doAppendDivider - Необходим ли пробел для разделения на колонки
 	* @return {string} - Элемент, дополненный пробелами
 */
-const addSpaces = function(data, requiredLength, notFirst) {
-	const needSpaces = requiredLength - data.length;
-	if(notFirst) {
-		data = ' ' + data;
+const addSpaces = function(data, requiredLength, doAppendDivider) {
+	let needSpacesCount = requiredLength - data.length;
+	if(doAppendDivider) {
+		needSpacesCount++;
 	}
-	for(let i = 0; i < needSpaces; i++) {
-		data = ' ' + data;
-	}
+	data = ' '.repeat(needSpacesCount) + data;
 	return data;
 }
 
 /**
 	* Форматирует массив элементов в строку
 	*
-	* @param {Array} input - Исходный массив
+	* @param {Array[number]} input - Исходный массив
 	* @param {number} columns - Количество колонок
 	* @return {string} - Отформатированная строка
 */
 const format = function(input, columns) {
-	const columnsLength = findLength(input, columns);
-	let output = '';	
+	const inputAsStrings = input.map(it => it.toString());
+	const columnsLengths = findLength(inputAsStrings, columns);
 	
-	for(let i = 0; i < input.length; i++) {
-		const col = i % columns;
-		output += addSpaces(String(input[i]), columnsLength[col], col > 0);
-		(col == columns - 1 && i < input.length -1) ? output += '\n' : '';
-	}
+	let output = '';
+	inputAsStrings.forEach((item, index) => {
+		const col = index % columns;
+		output += addSpaces(item, columnsLengths[col], col > 0);
+		
+		const isLastItemInRow = col == columns - 1;
+		const isLastItemEver = index == inputAsStrings.length -1;
+		if (isLastItemInRow && !isLastItemEver) {
+			output += '\n';
+		}
+	});
+
 	return output;
 }
